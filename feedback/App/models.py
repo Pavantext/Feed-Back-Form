1ddb3a79b93460a928012468da506a5a153a413e
Pavantext <pkabcd217@gmail.com>
2024-11-25 18:49:20 +0530

    Added Page Layout

--- feedback/App/models.py
+++ feedback/App/models.py
@@ -1,26 +1,79 @@
 from django.db import models
 
-# Create your models here.
-
 class Feedback(models.Model):
     name = models.CharField(max_length=100)
-    city = models.CharField(max_length=100, blank=True, null=True)
-    email = models.EmailField(blank=True)
-    mobile = models.CharField(max_length=12)
-    
-    
-    ticket_support = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    it_support = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    security_support = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    housekeeping = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    facilitators = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    archakas_guidance = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    devotion_experience = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    harathi_experience = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    fountain_laser_show = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    prasadam_feedback = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    revisit_feedback = models.CharField(max_length=20, choices=[('Strongly Agree', 'Strongly Agree'), ('Agree', 'Agree'), ('Neutral', 'Neutral'), ('Disagree', 'Disagree'), ('Strongly Disagree', 'Strongly Disagree')])
-    
+    city = models.CharField(max_length=100)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=15)
+
+  
+    ticket_support_strongly_agree = models.BooleanField(default=False)
+    ticket_support_agree = models.BooleanField(default=False)
+    ticket_support_neutral = models.BooleanField(default=False)
+    ticket_support_disagree = models.BooleanField(default=False)
+    ticket_support_strongly_disagree = models.BooleanField(default=False)
+
+    it_support_strongly_agree = models.BooleanField(default=False)
+    it_support_agree = models.BooleanField(default=False)
+    it_support_neutral = models.BooleanField(default=False)
+    it_support_disagree = models.BooleanField(default=False)
+    it_support_strongly_disagree = models.BooleanField(default=False)
+
+    security_team_strongly_agree = models.BooleanField(default=False)
+    security_team_agree = models.BooleanField(default=False)
+    security_team_neutral = models.BooleanField(default=False)
+    security_team_disagree = models.BooleanField(default=False)
+    security_team_strongly_disagree = models.BooleanField(default=False)
+
+    house_keeping_strongly_agree = models.BooleanField(default=False)
+    house_keeping_agree = models.BooleanField(default=False)
+    house_keeping_neutral = models.BooleanField(default=False)
+    house_keeping_disagree = models.BooleanField(default=False)
+    house_keeping_strongly_disagree = models.BooleanField(default=False)
+
+    facilitator_strongly_agree = models.BooleanField(default=False)
+    facilitator_agree = models.BooleanField(default=False)
+    facilitator_neutral = models.BooleanField(default=False)
+    facilitator_disagree = models.BooleanField(default=False)
+    facilitator_strongly_disagree = models.BooleanField(default=False)
+
+    archakas_strongly_agree = models.BooleanField(default=False)
+    archakas_agree = models.BooleanField(default=False)
+    archakas_neutral = models.BooleanField(default=False)
+    archakas_disagree = models.BooleanField(default=False)
+    archakas_strongly_disagree = models.BooleanField(default=False)
+
+    devotion_strongly_agree = models.BooleanField(default=False)
+    devotion_agree = models.BooleanField(default=False)
+    devotion_neutral = models.BooleanField(default=False)
+    devotion_disagree = models.BooleanField(default=False)
+    devotion_strongly_disagree = models.BooleanField(default=False)
+
+    harathi_strongly_agree = models.BooleanField(default=False)
+    harathi_agree = models.BooleanField(default=False)
+    harathi_neutral = models.BooleanField(default=False)
+    harathi_disagree = models.BooleanField(default=False)
+    harathi_strongly_disagree = models.BooleanField(default=False)
+
+    fountain_strongly_agree = models.BooleanField(default=False)
+    fountain_agree = models.BooleanField(default=False)
+    fountain_neutral = models.BooleanField(default=False)
+    fountain_disagree = models.BooleanField(default=False)
+    fountain_strongly_disagree = models.BooleanField(default=False)
+
+    prasadam_strongly_agree = models.BooleanField(default=False)
+    prasadam_agree = models.BooleanField(default=False)
+    prasadam_neutral = models.BooleanField(default=False)
+    prasadam_disagree = models.BooleanField(default=False)
+    prasadam_strongly_disagree = models.BooleanField(default=False)
+
+    visit_strongly_agree = models.BooleanField(default=False)
+    visit_agree = models.BooleanField(default=False)
+    visit_neutral = models.BooleanField(default=False)
+    visit_disagree = models.BooleanField(default=False)
+    visit_strongly_disagree = models.BooleanField(default=False)
+
+
 
     improvement_area = models.TextField(blank=True, null=True)
 
