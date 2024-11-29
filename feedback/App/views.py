112e6b902fbfc033fa9e2daa9911d5a61bf4f559
Pavantext <pavantext6@gmail.com>
2024-11-29 15:32:27 +0530

    Added CSS

--- feedback/App/views.py
+++ feedback/App/views.py
@@ -1,25 +1,5 @@
-# from django.shortcuts import render, redirect
-# from .forms import FeedbackForm
-
-# # Create your views here.
-
-# def feedback_form(request):
-#     if request.method == "POST":
-#         form = FeedbackForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return render(request, 'thank_you.html')
-#     else:
-#         form = FeedbackForm()
-#     return render(request, 'feedback_form.html', {'form': form})
-
-# # def thank_you(request):
-# #     return render(request, 'thank_you.html')
-
 from django.shortcuts import render, redirect
 from .forms import FeedbackForm
-
-
 
 
 FEEDBACK_QUESTIONS = {
