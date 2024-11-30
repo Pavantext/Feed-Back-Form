16359a7f2d23f7f83b479d7002e73e08872f39ef
Pavantext <pavantext6@gmail.com>
2024-11-30 21:21:53 +0530

    Added user_name feature

--- feedback/App/forms.py
+++ feedback/App/forms.py
@@ -7,30 +7,32 @@ class FeedbackForm(forms.ModelForm):
     city = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter city name'}), required=True)
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter Email address'}), required=True)
     mobile = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': 'Enter mobile number'}), max_length=15, required=True)
+    
+    CHOICES = [
+        ('strongly_agree', 'Strongly Agree'),
+        ('agree', 'Agree'),
+        ('neutral', 'Neutral'),
+        ('disagree', 'Disagree'),
+        ('strongly_disagree', 'Strongly Disagree'),
+    ]
+
+    ticket_support = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    it_support = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    security = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    housekeeping = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    facilitators = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    archakas = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    devotion = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    harathi = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    fountain = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    prasadam = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+    visit = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
+
+    improvement_areas = forms.CharField(widget=forms.Textarea, required=False)
+
+    rating = forms.CharField(widget=forms.Textarea, required=False)
+    quality = forms.CharField(widget=forms.Textarea, required=False)
+
     class Meta:
         model = Feedback
         fields = '__all__'
-        CHOICES = [
-            ('strongly_agree', 'Strongly Agree'),
-            ('agree', 'Agree'),
-            ('neutral', 'Neutral'),
-            ('disagree', 'Disagree'),
-            ('strongly_disagree', 'Strongly Disagree'),
-        ]
-
-        ticket_support = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        it_support = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        security = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        housekeeping = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        facilitators = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        archakas = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        devotion = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        harathi = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        fountain = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        prasadam = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-        visit = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=True)
-
-        improvement_areas = forms.CharField(widget=forms.Textarea, required=False)
-
-        rating = forms.CharField(widget=forms.Textarea, required=False)
-        quality = forms.CharField(widget=forms.Textarea, required=False)
--- feedback/App/views.py
+++ feedback/App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import FeedbackForm
+import uuid
 
 
 FEEDBACK_QUESTIONS = {
@@ -22,12 +23,21 @@ def feedback_form(request):
     if request.method == "POST":
         form = FeedbackForm(request.POST)
         if form.is_valid():
-            form.save()
-            return render(request, 'thank_you.html')
+            form_token = request.session.get('form_token', None)
+            submitted_token = request.POST.get('form_token', None)
+            if form_token and form_token == submitted_token:
+                feedback = form.save()
+                user_name = feedback.name
+                request.session['user_name'] = user_name
+                del request.session['form_token']
+                return render(request, 'thank_you.html', {'user_name': user_name})
+            else:
+                form.add_error(None, "This form has already submitted. You can Exit now.")  
     else:
         form = FeedbackForm()
+        request.session['form_token'] = str(uuid.uuid4())
     feedback_questions = {question: form[field] for question, field in FEEDBACK_QUESTIONS.items()}
-    return render(request, 'feedback_form.html', {'form': form, 'feedback_questions': feedback_questions})
+    return render(request, 'feedback_form.html', {'form': form, 'form_token': request.session['form_token'], 'feedback_questions': feedback_questions})
 
 
 def thank_you(request):
