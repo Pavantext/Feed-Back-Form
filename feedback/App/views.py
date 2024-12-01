f5fc26b8eacc4529a2213c33974ff64e2191d96f
Pavantext <pavantext6@gmail.com>
2024-12-01 20:17:24 +0530

    handeled errors

--- feedback/App/views.py
+++ feedback/App/views.py
@@ -22,9 +22,10 @@ FEEDBACK_QUESTIONS = {
 def feedback_form(request):
     if request.method == "POST":
         form = FeedbackForm(request.POST)
+        feedback_questions = {question: form[field] for question, field in FEEDBACK_QUESTIONS.items()}
         if form.is_valid():
-            form_token = request.session.get('form_token', None)
-            submitted_token = request.POST.get('form_token', None)
+            form_token = request.session.get('form_token')
+            submitted_token = request.POST.get('form_token')
             if form_token and form_token == submitted_token:
                 feedback = form.save()
                 user_name = feedback.name
@@ -32,13 +33,20 @@ def feedback_form(request):
                 del request.session['form_token']
                 return render(request, 'thank_you.html', {'user_name': user_name})
             else:
-                form.add_error(None, "This form has already submitted. You can Exit now.")  
-    else:
-        form = FeedbackForm()
-        request.session['form_token'] = str(uuid.uuid4())
-    feedback_questions = {question: form[field] for question, field in FEEDBACK_QUESTIONS.items()}
+                form.add_error(None, "This form has submitted succesfully. You can Exit now or refresh the page.")  
+        # else:
+        return render(request, 'feedback_form.html',
+            {'form': form,
+            'form_token': request.session.get('form_token', ''),
+            'feedback_questions': feedback_questions
+            })
+
+    request.session['form_token'] = str(uuid.uuid4())
+    form = FeedbackForm()
+    feedback_questions = {question: form[field] for question, field in FEEDBACK_QUESTIONS.items()} 
     return render(request, 'feedback_form.html', {'form': form, 'form_token': request.session['form_token'], 'feedback_questions': feedback_questions})
 
 
 def thank_you(request):
-    return render(request, 'thank_you.html')
+    user_name = request.session.get('user_name', 'User')
+    return render(request, 'thank_you.html', {'user_name': user_name})
